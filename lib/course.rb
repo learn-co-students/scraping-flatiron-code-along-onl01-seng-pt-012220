293f043645d37ec1e48895ccf70d2cd72aa8cda4
wynterwolf <shinyguns@gmail.com>
2020-02-20 16:44:22 -0600

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,21 @@
+class Course
 
+attr_accessor :title, :schedule, :description #getter and setters
+
+#set this for class method tests
+@@all = []
+
+def initialize 
+    @@all << self
+end
+
+def self.all
+    @@all
+end
+
+def self.reset_all
+    @@all.clear
+end
+#get_page
+end
 
